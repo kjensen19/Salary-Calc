e0d481ec018116f33ae7709b0efd18eeb62653b4
Kyle Jensen <kjensen19@gmail.com>
2022-08-26 15:07:01 -0500

    added input fields

--- scripts/client.js
+++ scripts/client.js
@@ -4,3 +4,16 @@ console.log('JS')
 function readyNow() {
     console.log('JQ')
 }
+
+
+
+
+let Employees = []
+let individualEmployee = {
+    firstName: 'Kyle',
+    lastName: 'Jensen',
+    idNumber: 7578,
+    jobTitle: 'overcomplicator',
+    annualSalary: '1'
+}
+
