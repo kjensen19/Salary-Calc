32c90efbf00893cdf504345de46c7321119d0549
Kyle Jensen <kjensen19@gmail.com>
2022-08-26 20:24:05 -0500

    employee creator func is working

--- scripts/client.js
+++ scripts/client.js
@@ -3,12 +3,13 @@ console.log('JS')
 
 function readyNow() {
     console.log('JQ')
+    $("#input-submit").on('click', employeeMaker)
 }
 
 
 
 
-let Employees = []
+let employees = []
 let individualEmployee = {
     firstName: 'Kyle',
     lastName: 'Jensen',
@@ -17,3 +18,21 @@ let individualEmployee = {
     annualSalary: '1'
 }
 
+function employeeMaker() {
+    //grab inputs to create new employee object
+    let newEmployee = {
+        firstName: $('#fName-input').val(),
+        lastName: $('#lName-input').val(),
+        idNumber: $('#id-input').val(),
+        jobTitle: $('#title-input').val(),
+        annualSalary: $('#salary-input').val()
+    }//end object
+    employees.push(newEmployee)
+    console.log(`Employees in func: ${employees[0].firstName}`)
+    //empty input fields
+    $('#lName-input').val('')
+    $('#id-input').val('')
+    $('#title-input').val('')
+    $('#salary-input').val('')
+    $('#fName-input').val('')
+}
